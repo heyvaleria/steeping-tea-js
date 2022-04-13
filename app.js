f9c473867f84f30170eed7d1659303c43e6f548a
Valeria Graffeo <valeria@thoughtbot.com>
2022-04-13 13:24:38 -0700

    ing songsrouter, using pg package, connect to localhost postgresql db, getting data back! yay

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
+const songsRouter = require('./routes/songs');
 const app = express();
 
 app.use(function(req, res, next) {
@@ -14,14 +13,8 @@ app.use(function(req, res, next) {
   next();
 });
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use('/api', express.static('api') , function(req, res){
-    // Optional 404 handler
-    res.status(404);
-    res.json({error:{code:404}})
-});
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -32,20 +25,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/api/v1/songs', songsRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
---
+++ routes/songs.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const { Client } = require('pg');
+
+const client = new Client({
+  user: 'valeria',
+  host: 'localhost',
+  database: 'steepingtea',
+  password: 'password',
+  port: 5432,
+});
+client.connect();
+
+
+router.get('/', function(req, res, next) {
+  client.query('SELECT * FROM songs', (err, response) => {
+    if (err) {
+      console.log(err.stack)
+      // make me better eventually
+    } else {
+      return res.json({songs: response.rows})
+    }
+  })
+});
+
+module.exports = router;
