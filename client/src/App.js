925898549c8310835523fad46f08766a345639d7
Valeria Graffeo <valeria@thoughtbot.com>
2022-04-14 10:20:39 -0700

    Added Formik for add a song form on frontend side

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import SongsContainer from './components/SongsContainer'
+import NewSongForm from './components/NewSongForm'
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <div>
       	  <h1>Steeping Tea</h1>
         </div>
+        <NewSongForm />
 	      <SongsContainer />
       </div>
     );
---
+++ client/src/components/NewSongForm.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+const NewSongForm = () => (
+  <div>
+    <p>Add a song</p>
+    <Formik
+      initialValues={{ artist: '', title: '', url: '', duration: 0 }}
+      validate={values => {
+        const errors = {};
+        if (!values.url) {
+          errors.url = 'Required';
+        } else if (
+          !RegExp('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?').test(values.url)
+        ) {
+          errors.url = 'Invalid URL format';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="artist" name="artist" />
+          <Field type="title" name="title" />
+          <Field type="url" name="url" />
+          <ErrorMessage name="url" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+export default NewSongForm;
