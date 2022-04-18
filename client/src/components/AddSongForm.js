fb8ffe1786b712f6fc8f27ca30b1078ee63ff6e2
Valeria Graffeo <valeria@thoughtbot.com>
2022-04-18 10:55:55 -0700

    of course not, the p tag cannot be the descendant of the div, I knew that

--- client/src/components/AddSongForm.js
+++ client/src/components/AddSongForm.js
@@ -10,13 +10,11 @@ const CustomInput = ({ label, ...props }) => {
    const [field, meta] = useField(props);
    return (
      <>
-      <p>
-         <label htmlFor={props.id || props.name}>{label}</label>
-         <input className="text-input" {...field} {...props} />
-         {meta.touched && meta.error ? (
-           <div className="error">{meta.error}</div>
-         ) : null}
-       </p>
+       <label htmlFor={props.id || props.name}>{label}</label>
+       <input className="text-input" {...field} {...props} />
+       {meta.touched && meta.error ? (
+         <div className="error">{meta.error}</div>
+       ) : null}
      </>
    );
  };
