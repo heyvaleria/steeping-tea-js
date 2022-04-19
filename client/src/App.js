a2e0ecc8148c02d867eee0b209c756ccc84f9745
Valeria Graffeo <valeria@thoughtbot.com>
2022-04-19 08:09:04 -0700

    make App.js file a function not a class in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
 import SongsContainer from './components/SongsContainer'
 import AddSongForm from './components/AddSongForm'
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <div>
-      	  <h1>Steeping Tea</h1>
-        </div>
-        <AddSongForm />
-	      <SongsContainer />
+const App = () => {
+  return (
+    <div className='App'>
+      <div>
+        <h1>Steeping Tea</h1>
       </div>
-    );
-  }
+      <AddSongForm />
+      <SongsContainer />
+    </div>
+  );
 }
 
 export default App
